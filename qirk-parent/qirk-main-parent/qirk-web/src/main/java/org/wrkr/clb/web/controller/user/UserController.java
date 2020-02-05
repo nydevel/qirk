f8d39573fe1298217c9d5ff747b4cb1ce24eb9cc
Evgeny <zhenyaporeykin@yandex.ru>
2020-02-05 16:41:48 +0500

    changed license

--- qirk-parent/qirk-main-parent/qirk-web/src/main/java/org/wrkr/clb/web/controller/user/UserController.java
+++ qirk-parent/qirk-main-parent/qirk-web/src/main/java/org/wrkr/clb/web/controller/user/UserController.java
@@ -3,7 +3,7 @@
  */
 /*
  * This file is part of the Java API to Qirk.
- * Copyright (C) 2020 Memfis LLC, Russia
+ * Copyright (C) 2020 Memfis Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of
  * the GNU General Public License as published by the Free Software Foundation, either version 3
