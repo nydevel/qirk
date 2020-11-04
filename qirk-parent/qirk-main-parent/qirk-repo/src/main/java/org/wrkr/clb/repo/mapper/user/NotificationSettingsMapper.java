167163ea5c65139c357e2a8f1e010e67bfeecfd9
Nick Yasinskiy <1@shfu.ru>
2020-11-04 10:11:40 +0500

    Preparing for github publishing

--- qirk-parent/qirk-main-parent/qirk-repo/src/main/java/org/wrkr/clb/repo/mapper/user/NotificationSettingsMapper.java
+++ qirk-parent/qirk-main-parent/qirk-repo/src/main/java/org/wrkr/clb/repo/mapper/user/NotificationSettingsMapper.java
@@ -1,19 +1,3 @@
-/*
- * This file is part of the Java API to Qirk.
- * Copyright (C) 2020 Memfis Inc.
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
- *
- */
 package org.wrkr.clb.repo.mapper.user;
 
 import java.sql.ResultSet;
